a1bce3f61c7a447159b32ddd809e073bcfa779e4
goncalveshigino <goncalveshigono20@gmail.com>
2021-01-04 18:56:48 +0100

    Alteracoes na oferta service

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,19 +1,18 @@
-import {Http} from '@angular/http';
-import { Injectable } from '@angular/core';
-import {Oferta} from './shared/oferta.model';
+import { Oferta } from './shared/oferta.model'
+import { Http} from '@angular/http';
+import  {Injectable} from '@angular/core';
 
-import 'rxps/add/operator/toPromise';
+import 'rxjs/add/operator/toPromise'
 
 
 @Injectable()
 export class OfertasService {
- 
-  constructor(private  http: Http){}
 
-  public getOfertas(): Promise<Oferta[]> {
-    return this.http.get('http://localhost:30/ofertas')
-    .toPromise()
-    .then((resposta: any)=> resposta.json())
-  }
- 
+  constructor(private http: Http){}
+
+    public getOfertas(): Promise<Oferta[]> {
+        return this.http.get("http://localhost:3000/ofertas?destaque=true")
+        .toPromise().then((resposta: any) => resposta.json());
+    }
+
 }
