897354b8024ed94a2b37afd92463c8e38c49a710
goncalveshigino <goncalveshigono20@gmail.com>
2021-01-07 22:59:57 +0100

    Add navegação e dei uma correção nas rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
+import { OfertaComponent } from './oferta/oferta.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { DiversaoComponent } from './diversao/diversao.component';
     HomeComponent,
     RodapeComponent,
     RestaurantesComponent,
-    DiversaoComponent
+    DiversaoComponent,
+    OfertaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,23 +2,34 @@ import { Oferta } from './shared/oferta.model'
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+import {URL_API} from './app.api'
+
 import 'rxjs/add/operator/toPromise'
 
 
 @Injectable()
 export class OfertasService {
 
+  
+
   constructor(private http: Http) { }
 
   public getOfertas(): Promise<Oferta[]> {
-    return this.http.get("http://localhost:3000/ofertas?destaque=true")
+    return this.http.get(`${URL_API}?destaque=true`)
       .toPromise().then((resposta: any) => resposta.json());
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
-    .toPromise()
-    .then( (resposta: any ) => resposta.json())
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
+      .toPromise()
+      .then((resposta: any) => resposta.json())
   }
 
+  public getOfertaPorId(id: number): Promise<Oferta> {
+    return this.http.get(`${URL_API}?id=${id}`)
+      .toPromise()
+      .then((respota: any) => {
+        return respota.json()[0]
+      })
+  }
 }
