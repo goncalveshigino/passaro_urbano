b8fad3532c8cafe6239cfc09c06b20f6ff2fb274
goncalveshigino <goncalveshigono20@gmail.com>
2021-01-07 16:17:21 +0100

    atualizando as rotas

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,6 @@
 import { Oferta } from './shared/oferta.model'
-import { Http} from '@angular/http';
-import  {Injectable} from '@angular/core';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise'
 
@@ -8,11 +8,17 @@ import 'rxjs/add/operator/toPromise'
 @Injectable()
 export class OfertasService {
 
-  constructor(private http: Http){}
+  constructor(private http: Http) { }
 
-    public getOfertas(): Promise<Oferta[]> {
-        return this.http.get("http://localhost:3000/ofertas?destaque=true")
-        .toPromise().then((resposta: any) => resposta.json());
-    }
+  public getOfertas(): Promise<Oferta[]> {
+    return this.http.get("http://localhost:3000/ofertas?destaque=true")
+      .toPromise().then((resposta: any) => resposta.json());
+  }
+
+  public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    .toPromise()
+    .then( (resposta: any ) => resposta.json())
+  }
 
 }
