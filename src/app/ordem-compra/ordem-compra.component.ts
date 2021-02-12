0db34b31c3f4ea0b7af4873d1c475c78bac3d622
goncalveshigino <goncalveshigono20@gmail.com>
2021-02-12 11:29:18 +0100

    feito com sucess

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public address: string = '';
+  public number: string = '';
+  public complement: string = '';
+  public formPagyment: string = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public atualizaEndereco(address: string): void {
+    this.address = address
+    console.log(this.address)
+  }
+
+  public atualizaNumero(number: string): void {
+    this.number = number
+    console.log(this.number)
+  }
+
+  public atualizaComplemento(complement: string): void {
+    this.complement = complement
+    console.log(this.complement)
+  }
+
+  public atualizaFormPayment(formPagyment: string): void {
+    this.formPagyment = formPagyment
+    console.log(this.formPagyment)
   }
 
 }
